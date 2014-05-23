7a9e05c2f2182874074ba23de4a44cffc96f4036
Konstantin Zeldovich <konstantin.zeldovich@umassmed.edu>
2014-05-23 15:05:20 -0400

    updated examples

--- Makefile
+++ Makefile
@@ -1,12 +1,3 @@
-#cd src
-#nvcc -arch=sm_30 -c kernels.cu
-#nvcc -arch=sm_30 -lcublas -c galelib.cu
-#cd ../examples
-#g++ -I../src -c main.cpp
-#g++ main.o ../src/galelib.o ../src/kernels.o -o galeprot-test -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas
-#./galeprot-test ../data/MJ96_letters.d ../data/c-10k-64.d 10000 ../data/faces-10k-64.d 10000 4 ../data/seq64_many.in 8192 ../data/bind_pairs.in 10000 fold.out bind.out
-
-
 NVCC=nvcc
 NVCCARCH=-arch=sm_30
 NVCCFLAGS=$(NVCCARCH) -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas -lcudadevrt
@@ -24,12 +15,10 @@ galeprotlib: $(SRCDIR)kernels.cu $(SRCDIR)galelib.cu
 	mv libgaleprot.a lib
 	rm -f *.o *.a
 #	rm kernels.o galelib.o
-	g++ -Iinclude -c main.cpp
-	g++ -o runme main.o -Llib -lgaleprot -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas -lcudadevrt
+#	g++ -Iinclude -c main.cpp
+#	g++ -o runme main.o -Llib -lgaleprot -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas -lcudadevrt
 #	g++ -o runme main.o kernels.o galelib.o -L. -lgaleprot -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas -lcudadevrt
 	
-
-
 install:
 	mkdir include
 	cp $(SRCDIR)/galelib.h include
--- examples/main.cpp
+++ examples/main.cpp
@@ -1,41 +1,47 @@
 #include "galelib.h"
 #include <time.h>
-// some stuff is already included in galelib.h ...
+
+
 #define MAX(x,y) (((x)>(y))?(x):(y))
 #define MIN(x,y) (((x)<(y))?(x):(y))
-//
+
 double diff(timespec start, timespec end);
-inline unsigned char LetterToMJCode(char AAin);
+unsigned char LetterToMJCode(char AAin);
 int ReadFileWithSequences(const char *filename, unsigned char **ArrayOfSequences, int *MaxSequenceLength);
 int ReadFileWithBindingPairs(const char *filename, int **bind_pairs);
-//
+
 int main(int argc, char **argv) {
-    //
+
     timespec start, stop;
-    int fold_wsize, bind_wsize;
-    int num_conf, num_conf_bind, edge_len;
-    //
-    char *f_field_fname = argv[1];
-    char *str_file_name = argv[2];
-    if (sscanf(argv[3],"%d",&num_conf) != 1) { fprintf(stderr, "3rd arg is NumConf as int\n"); exit(1); }
-    char *faces_b_fname = argv[4];
-    if (sscanf(argv[5],"%d",&num_conf_bind) != 1) { fprintf(stderr, "5rd arg is NumConfBind as int\n"); exit(1); }
-    if (sscanf(argv[6],"%d",&edge_len) != 1) { fprintf(stderr, "6rd arg is EdgeLen as int\n"); exit(1); }
-    // in/out stuff ...
-    char *seq_file_name = argv[7];
-    if (sscanf(argv[8],"%d",&fold_wsize) != 1) { fprintf(stderr, "8th arg is FoldChunkSize as int\n"); exit(1); }
-    char *bind_filename = argv[9];
-    if (sscanf(argv[10],"%d",&bind_wsize) != 1) { fprintf(stderr, "10th arg is BindChunkSize as int\n"); exit(1); }
-    char *fold_out_fname = argv[11];
-    char *bind_out_fname = argv[12];
-    // some checks ...
+    
+    char f_field_fname[] = "../data/MJ96_letters.d";
+    char str_file_name[] = "../data/c-10k-64.d";
+    int num_conf = 10000; // 10,000 candidate conformations in c-10k-64.d
+    int fold_wsize=10000; // fold 10,000 sequences at a time
+    
+    char faces_b_fname[] = "../data/faces-10k-64.d";
+    int num_conf_bind = 10000; // 10,000 conformations in faces-10k-64.d
+    int edge_len = 4; // cube size, 4*4*4=64-mers
+    int bind_wsize=10000; // bind 10,000 pairs of sequences at a time
+
+    // input sequences
+    char seq_file_name[]="../data/seq64_many.in";
+   
+    // pairs of sequences to bind 
+    char bind_filename[] = "../data/bind_pairs.in";
+         
+    // output files
+    char fold_out_fname[] = "fold.out";
+    char bind_out_fname[] = "bind.out";
+    
+    // sanity check
     if (num_conf != num_conf_bind) { fprintf(stderr, "NumConf must be equal to NumConfBind\n"); exit(1); }
-    //
-    // allocate some GALE struct ...
+
+    // allocate GALE struct, the GPU
     GALE_Data *data_ptr;
     gale_init_null(&data_ptr,0);
-    //
-    // allocate sequences (load from somewhere ...) and binding pairs ...
+
+    // allocate sequences, binding pairs and load from files
     unsigned char *seqarray;
     int *bind_pairs;
     int seqlen, num_seq = ReadFileWithSequences(seq_file_name, &seqarray, &seqlen);
@@ -43,22 +49,23 @@ int main(int argc, char **argv) {
     // conf_pairs: just allocating and setting to 0s, later pick up native folds ... 
     int *conf_pairs = (int *)malloc(2*num_pairs*sizeof(int)); assert(conf_pairs != NULL);
     memset(conf_pairs, 0, 2*num_pairs*sizeof(int));
+
     // allocate pnat & inat arrays, binding array to be allocated too ...
     float   *pnat = (float *)malloc(num_seq*sizeof(float));     assert(pnat != NULL);
     int     *inat = (int *)malloc(num_seq*sizeof(int));         assert(inat != NULL);
     float   *ebind = (float *)malloc(num_pairs*sizeof(float)); assert(ebind != NULL);
     float   *pbind = (float *)malloc(num_pairs*sizeof(float)); assert(pbind != NULL);
     int     *ibind = (int *)malloc(num_pairs*sizeof(int));     assert(ibind != NULL);
-    //
-    // load structural info, faces info & force field ...
+    
+    // load structural info, faces info & force field
     gale_load_forcefield(data_ptr, f_field_fname);
     gale_load_contactmap(data_ptr, str_file_name, num_conf);
     gale_load_bind_faces(data_ptr, faces_b_fname, num_conf, edge_len);
-    //
+    
     // define worksize (this must precede all other "set" type of functions)...
     gale_set_fold_worksize(data_ptr, fold_wsize);
     gale_set_bind_worksize(data_ptr, bind_wsize);
-    //
+    
     // connect seqarray to the device ...
     gale_set_seqarray(data_ptr, seqarray, seqlen, num_seq);
     gale_set_bind_pairs(data_ptr, bind_pairs, conf_pairs, num_pairs);
@@ -67,25 +74,31 @@ int main(int argc, char **argv) {
     gale_set_pbind(data_ptr, pbind);
     gale_set_ibind(data_ptr, ibind);
     gale_set_ebind(data_ptr, ebind);
-    // prepare & fold ...
+    
+    // prepare for folding (allocates GPU arrays)
     gale_fold_prepare(data_ptr);
+
     clock_gettime(CLOCK_REALTIME, &start);
     gale_fold_compute(data_ptr, 0.8);
     clock_gettime(CLOCK_REALTIME, &stop);
     //
     {
         double dt = diff(start, stop);
-        printf("\nFolded %d seq in %.3f sec. %.2f kfps\n",num_seq, dt, ((float)num_seq)/dt/1000.0f);
+        printf("\nFolded %d sequences in %.3f sec, %.2f kfps\n",num_seq, dt, ((float)num_seq)/dt/1000.0f);
     }
-    //
-    //
-    // fill in conf_pairs with actual native folds ...
+
+    // free GPU device memory
+    gale_fold_unprepare(data_ptr);
+
+
+    // Before binding calculation, fill in conf_pairs with actual native folds ...
     for (int i = 0; i < 2*num_pairs; i++) {
         conf_pairs[i] = inat[bind_pairs[i]];
     }
+
     // prepare for bind & bind
-    gale_fold_unprepare(data_ptr);
     gale_bind_prepare(data_ptr);
+
     clock_gettime(CLOCK_REALTIME, &start);
     gale_bind_compute(data_ptr, 0.8);
     clock_gettime(CLOCK_REALTIME, &stop);
@@ -95,7 +108,6 @@ int main(int argc, char **argv) {
         printf("\nBinded %d seq pairs in %.3f sec. %.2f kbps\n",num_pairs, dt, ((float)num_pairs)/dt/1000.0f);
     }
 
-    //
     // some simple fold output ...
     {
         FILE *fp = fopen(fold_out_fname,"w");
@@ -108,8 +120,8 @@ int main(int argc, char **argv) {
         for (int i = 0; i < num_pairs; i++) { fprintf(fp, "%d %d %d %d %f %d %f\n", bind_pairs[2*i+0], bind_pairs[2*i+1], conf_pairs[2*i+0], conf_pairs[2*i+1], ebind[i], ibind[i], pbind[i]); } 
         fclose(fp);
     }
-    //
-    // freeing all allocated memory (user & API)...
+
+    // free allocated memory
     if(bind_pairs != NULL) { free(bind_pairs); }
     if(conf_pairs != NULL) { free(conf_pairs); }
     if(seqarray != NULL) { free(seqarray); }
@@ -118,8 +130,10 @@ int main(int argc, char **argv) {
     if(pbind != NULL) { free(pbind); }
     if(ebind != NULL) { free(ebind); }
     if(ibind != NULL) { free(ibind); }
+
+    // free memory allocated on the GPU
     gale_close(&data_ptr);
-    //
+
     return 0;
 }
 
@@ -127,7 +141,7 @@ int main(int argc, char **argv) {
 
 
 
-inline unsigned char LetterToMJCode(char AAin) {
+unsigned char LetterToMJCode(char AAin) {
     char AAandZ[] = "CMFILVWYAGTSNQDEHRKPZ";
     unsigned char aa_index = 0;
     // search a match from AAorder, 
@@ -183,8 +197,6 @@ int ReadFileWithSequences(const char *filename, unsigned char **ArrayOfSequences
 }
 
 
-
-
 int ReadFileWithBindingPairs(const char *filename, int **bind_pairs){
     FILE *fp = fopen(filename,"r");
     if (fp == NULL) { fprintf(stderr, "no file with binding pairs ...\n"); exit(1); }
@@ -210,7 +222,6 @@ int ReadFileWithBindingPairs(const char *filename, int **bind_pairs){
 }
 
 
-
 double diff(timespec start, timespec end) {
     timespec temp;
     if ((end.tv_nsec-start.tv_nsec)<0) {
@@ -220,35 +231,5 @@ double diff(timespec start, timespec end) {
         temp.tv_sec = end.tv_sec-start.tv_sec;
         temp.tv_nsec = end.tv_nsec-start.tv_nsec;
     }
-    // return temp;
     return temp.tv_sec + temp.tv_nsec / 1000000000.;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
