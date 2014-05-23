dade9da444f3e236eb43a9cbb0fec6a3290689a3
Konstantin Zeldovich <konstantin.zeldovich@umassmed.edu>
2014-05-23 13:20:25 -0400

    fix test case

--- runme-test.sh
+++ runme-test.sh
@@ -1,12 +1,9 @@
+cd src
 nvcc -arch=sm_30 -c kernels.cu
-echo "kernels are compiled"
 nvcc -arch=sm_30 -lcublas -c galelib.cu
-echo "cuda wrappers are compiled"
-g++ -c main.cpp
-echo "example cpp code is compiled"
-g++ main.o galelib.o kernels.o -o build/run -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas
-echo "everything is linked and ready to go ..."
-rm *.o
-echo "object files are deleted to make dir look cleaner ..."
 
-# ./build/run MJ96_letters.d c-10k-64.d 10000 faces-10k-64.d 10000 4 sequences64.in 8192 bind_pairs.in 10000 fold.out bind.out
+cd ../examples
+g++ -I../src -c main.cpp
+g++ main.o ../src/galelib.o ../src/kernels.o -o galeprot-test -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas
+
+./galeprot-test ../data/MJ96_letters.d ../data/c-10k-64.d 10000 ../data/faces-10k-64.d 10000 4 ../data/seq64_many.in 8192 ../data/bind_pairs.in 10000 fold.out bind.out
